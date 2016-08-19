068cd8eb137d9f6782c441d5f92168877cf50bbc
vitduck <levietduc@kaist.ac.kr>
2016-08-19 19:13:05 +0900

    Add ASS sub extraction

--- Subtitle.pm
+++ Subtitle.pm
@@ -4,6 +4,9 @@ package Subtitle;
 use autodie; 
 use warnings FATAL => 'all'; 
 
+# core 
+use File::Basename; 
+
 # cpan
 use Moose::Role;  
 use namespace::autoclean; 
@@ -23,5 +26,20 @@ has 'sub_id', (
         return $self->select_stream('Subtitle', $self->subtitle); 
     },  
 );   
+
+has 'ass', ( 
+    is       => 'ro', 
+    isa      => 'Str', 
+    lazy     => 1, 
+    init_arg => undef, 
+
+    default  => sub ( $self ) { 
+        return my $ass = $self->name =~ s/(.*)\..+?$/$1.ass/r; 
+    },  
+); 
+
+sub extract_sub ( $self ) { 
+    system "ffmpeg -y -loglevel fatal  -i ${\$self->name} -map 0:${\$self->sub_id} ${\$self->ass}"; 
+} 
 
 1;  
